46a3efad2b7ad749f146992a8ee51839fdb37eb1
azdrojowa248896 <azdrojowa248896@e-science.pl>
2021-10-16 01:02:53 +0200

    fix: display pins with SOR

--- src/CityMap.jsx
+++ src/CityMap.jsx
@@ -1,46 +1,71 @@
-import React,{ useState } from 'react'
-import MapGL, {GeolocateControl } from 'react-map-gl'
+//<a href='https://dryicons.com/free-icons/medical'> Icon by Dryicons </a>
+import React, {useState} from 'react'
+import ReactMapGL, {GeolocateControl, Marker} from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import * as config from "./config";
+import {Wroclaw} from "./data/Wroclaw";
+import {makeStyles} from "@material-ui/core";
 
-const TOKEN=config.TOKEN;
+const TOKEN = config.TOKEN;
+
+const useStyles = makeStyles(() => ({
+    markerButton: {
+        background: "none",
+        border: "none",
+        cursor: "pointer"
+    },
+    svgButton: {
+        width: '20px',
+        height: '20px',
+    }
+}));
 
 const geolocateStyle = {
     float: 'left',
     margin: '50px',
     padding: '10px'
-};
+}
+export default function Map() {
 
-const Map = () => {
+    const classes = useStyles();
 
-    const [viewport, setViewPort ] = useState({
-        width: "100whs",
+    const [viewport, setViewPort] = useState({
+        width: "100wh",
         height: '100vh',
         latitude: 0,
         longitude: 0,
         zoom: 2
     })
 
-    const _onViewportChange = viewport => setViewPort({...viewport, transitionDuration: 3000 })
+    const _onViewportChange = viewport => setViewPort({...viewport, transitionDuration: 3000})
 
     return (
-        <div style={{ margin: '0 auto'}}>
-            <h1 style={{textAlign: 'center', fontSize: '25px', fontWeight: 'bolder' }}>GeoLocator: Click the Geolocator to Find Your Location</h1>
 
-            <MapGL
+        <div style={{margin: '0 auto'}}>
+            <h1 style={{textAlign: 'center', fontSize: '25px', fontWeight: 'bolder'}}>GeoLocator: Click the Geolocator
+                to Find Your Location</h1>
+            <ReactMapGL
                 {...viewport}
                 mapboxApiAccessToken={TOKEN}
-                mapStyle="mapbox://styles/mapbox/dark-v8"
                 onViewportChange={_onViewportChange}
             >
+                {
+                    Wroclaw.map(sor => (
+                        <Marker key={sor.ID} longitude={sor.geometry.coordinates[1]}
+                                latitude={sor.geometry.coordinates[0]}>
+                            <button className={classes.markerButton}>
+                                <img className={classes.svgButton} src="/medicine.svg" alt="SOR"/>
+                            </button>
+                        </Marker>
+                    ))
+                }
                 <GeolocateControl
                     style={geolocateStyle}
                     positionOptions={{enableHighAccuracy: true}}
                     trackUserLocation={true}
                 />
-            </MapGL>
+            </ReactMapGL>
         </div>
     )
 }
 
-export default Map
---
+++ src/data/Wroclaw.js
@@ -0,0 +1,43 @@
+export const Wroclaw =
+   [
+    {
+      "ID": 1,
+      "name": "SOR Borowska",
+       "phone": " 71 733 29 64",
+        "address": "ul. Borowska 213",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [51.07615612663739, 17.03150761952651]
+      }
+    },
+    {
+      "ID":2,
+      "name": "SOR 4WSKzP",
+        "phone": "26 166 02 22",
+        "address": " ul. Weigla 5",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [51.07650582448226, 17.02372812155103]
+      }
+    },
+    {
+      "ID": 3,
+      "name": "SOR Kamieńskiego",
+        "phone":"71 32 70 115",
+        "address": "ul. Kamieńskiego 73a",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [51.14839133640592, 17.054013111674443]
+      }
+    },
+    {
+      "ID": 4,
+      "name": "Szpitalny oddział ratunkowy. Szpital im. T Marciniaka",
+      "phone": "713064304",
+        "address": "ul. Fieldorfa 2",
+        "geometry": {
+        "type": "Point",
+        "coordinates": [51.146346064950166, 16.92455733108644]
+      }
+    }
+  ]
