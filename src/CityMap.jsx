6ea24bac0f77302d3010fa2cc6eeac8587fe4116
azdrojowa248896 <azdrojowa248896@e-science.pl>
2021-10-16 02:20:24 +0200

    fix: add Searching and PopUp window

--- src/CityMap.jsx
+++ src/CityMap.jsx
@@ -1,12 +1,26 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
 import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import React, { useRef, useState } from 'react';
-import MapGL from 'react-map-gl';
+import ReactMapGL, {Marker, Popup} from 'react-map-gl';
 import DeckGL, { GeoJsonLayer } from 'deck.gl';
 import Geocoder from 'react-map-gl-geocoder';
 import * as config from './config';
+import {data} from "./data";
+import {makeStyles} from "@material-ui/core";
 
 const token = config.TOKEN;
+
+const useStyles = makeStyles(() => ({
+    markerButton: {
+        background: "none",
+        border: "none",
+        cursor: "pointer"
+    },
+    svgButton: {
+        width: '20px',
+        height: '20px',
+    }
+}));
 
 const SearchableMap = () => {
   const [viewport, setViewPort] = useState({
@@ -15,8 +29,9 @@ const SearchableMap = () => {
     zoom: 6.3,
     transitionDuration: 100,
   });
+  const [selectedSOR, setSelectedSOR] = useState(null);
   const [searchResultLayer, setSearchResult] = useState(null);
-
+    const classes = useStyles();
   const mapRef = useRef();
 
   const handleOnResult = (event) => {
@@ -48,9 +63,9 @@ const SearchableMap = () => {
       <h1
         style={{ textAlign: 'center', fontSize: '25px', fontWeight: 'bolder' }}
       >
-        Use the search bar to find a location on the map
+        Skorzystaj z wyszukiwarki w celu znalezienia swojego miasta
       </h1>
-      <MapGL
+      <ReactMapGL
         ref={mapRef}
         {...viewport}
         mapStyle="mapbox://styles/mapbox/streets-v9"
@@ -59,6 +74,29 @@ const SearchableMap = () => {
         onViewportChange={setViewPort}
         mapboxApiAccessToken={token}
       >
+          {
+              data.map(sor => (
+                  <Marker key={sor.ID} longitude={sor.geometry.coordinates[1]}
+                          latitude={sor.geometry.coordinates[0]}>
+                      <button className={classes.markerButton} onClick={(e) => {
+                          e.preventDefault();
+                          setSelectedSOR(sor);
+                      }}>
+                          <img className={classes.svgButton} src="/medicine.svg" alt="SOR"/>
+                      </button>
+                  </Marker>
+              ))
+          }
+          {selectedSOR ? (
+            <Popup latitude={selectedSOR.geometry.coordinates[0]} longitude={selectedSOR.geometry.coordinates[1]}>
+                <div>
+                    <h5>{selectedSOR.name}</h5>
+                    <h6>Numer telefonu:  {selectedSOR.phone}</h6>
+                    <h6>Aktaulna liczba pacjentów czekających na SOR:  </h6>
+                    <h6>Przewidywany czas ocziekiwania:  </h6>
+                </div>
+            </Popup>
+          ):null}
         <Geocoder
           mapRef={mapRef}
           onResult={handleOnResult}
@@ -68,8 +106,7 @@ const SearchableMap = () => {
           placeholder="Szukaj"
           countries="PL"
         />
-      </MapGL>
-      <DeckGL {...viewport} layers={[searchResultLayer]} />
+      </ReactMapGL>
     </div>
   );
 };
---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const data =
+    [
+        {
+            "ID": 1,
+            "name": "SOR Borowska",
+            "phone": "71 7332964",
+            "address": "ul. Borowska 213",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [51.07615612663739, 17.03150761952651]
+            }
+        },
+        {
+            "ID":2,
+            "name": "SOR 4WSKzP",
+            "phone": "26 1660222",
+            "address": " ul. Weigla 5",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [51.07650582448226, 17.02372812155103]
+            }
+        },
+        {
+            "ID": 3,
+            "name": "SOR Kamieńskiego",
+            "phone":"71 3270115",
+            "address": "ul. Kamieńskiego 73a",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [51.14839133640592, 17.054013111674443]
+            }
+        },
+        {
+            "ID": 4,
+            "name": "Szpitalny oddział ratunkowy. Szpital im. T Marciniaka",
+            "phone": "71 3064304",
+            "address": "ul. Fieldorfa 2",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [51.146346064950166, 16.92455733108644]
+            }
+        }
+    ]
