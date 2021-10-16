a3c5b53dd21f01becee5ddd65535eba1301cb50a
azdrojowa248896 <azdrojowa248896@e-science.pl>
2021-10-16 12:39:56 +0200

    fix: initial

--- src/CityMap.jsx
+++ src/CityMap.jsx
@@ -6,7 +6,11 @@ import {GeoJsonLayer} from 'deck.gl';
 import Geocoder from 'react-map-gl-geocoder';
 import * as config from './config';
 import {data} from "./data";
-import {makeStyles} from "@material-ui/core";
+import {makeStyles, Typography} from "@material-ui/core";
+import PhoneIcon from '@material-ui/icons/LocalPhone';
+import BusinessIcon from '@material-ui/icons/Business';
+import Divider from '@material-ui/core/Divider';
+import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
 
 const token = config.TOKEN;
 
@@ -17,8 +21,8 @@ const useStyles = makeStyles(() => ({
         cursor: "pointer"
     },
     svgButton: {
-        width: '30px',
-        height: '30px',
+        width: '40px',
+        height: '40px',
     }
 }));
 
@@ -33,6 +37,7 @@ const SearchableMap = () => {
     const [searchResultLayer, setSearchResult] = useState(null);
     const classes = useStyles();
     const mapRef = useRef();
+    const times = ['1,5 h temu', '1 h temu', '0,5 h temu', 'teraz'];
 
     const handleOnResult = (event) => {
         setSearchResult(
@@ -58,7 +63,6 @@ const SearchableMap = () => {
     };
 
     async function readInfoFromSOR(sor) {
-        //const responseSOR = await fetch("http://localhost:3000/api/v1/data")
         return await fetch(`http://localhost:3000/api/v1/data/${sor.ID}`, {
             method: 'GET',
             mode: 'cors',
@@ -93,9 +97,12 @@ const SearchableMap = () => {
                                 e.preventDefault();
                                 await readInfoFromSOR(sor).then(response => {
                                     response.json().then(p => {
-                                        const length = p.data.data.length;
-                                        sor.patients = p.data.data[length - 1].queueLength;
-                                        sor.estimatedTime = p.data.data[length - 1].expectedWaitTime;
+                                        sor.patients = p.data.data[3].queueLength;
+                                        sor.estimatedTime = p.data.data[3].expectedWaitTime;
+                                        sor.halfHour = p.data.data[2].queueLength;
+                                        sor.hour = p.data.data[1].queueLength;
+                                        sor.oneAndHalf = p.data.data[0].queueLength;
+                                        sor.queueLengths = [p.data.data[3].queueLength, p.data.data[2].queueLength, p.data.data[1].queueLength, p.data.data[0].queueLength  ]
                                         setSelectedSOR(sor);
                                     })
                                 })
@@ -106,22 +113,105 @@ const SearchableMap = () => {
                     ))
                 }
                 {selectedSOR ? (
-                    <Popup latitude={selectedSOR.coordinates[0]}
-                           longitude={selectedSOR.coordinates[1]}
-                           closeOnClick={true}
-                           onClose={closePopUp}>
-                        <div>
-                            <h5>{selectedSOR.name}</h5>
-                            <h6>Numer telefonu: {selectedSOR.phone}</h6>
-                            <h6>Aktaulna liczba pacjentów czekających na SOR: {selectedSOR.patients} </h6>
-                            <h6>Przewidywany czas ocziekiwania: {selectedSOR.estimatedTime}</h6>
-                            <h6>Historia liczby pacjentów w punkcie SOR: </h6>
-                            <ul>
-                                <li><h6>0,5h temu: </h6></li>
-                                <li><h6>1h temu: </h6></li>
-                                <li><h6>1,5h temu: </h6></li>
-                            </ul>
+                    <Popup
+                        latitude={selectedSOR.coordinates[0]}
+                        longitude={selectedSOR.coordinates[1]}
+                        closeOnClick={true}
+                        onClose={closePopUp}
+                    >
+                        <div
+                            style={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'start',
+                                justifyContent: 'center',
+                                padding: 20,
+                            }}
+                        >
+                            <Typography variant="h5" style={{ marginBottom: 30 }}>
+                                {selectedSOR.name}
+                            </Typography>
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    margin: 10,
+                                }}
+                            >
+                                <PhoneIcon style={{ marginRight: 10 }} />
+                                <Typography variant="h7">{selectedSOR.phone}</Typography>
+                            </div>
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    margin: 10,
+                                    marginBottom: 30,
+                                }}
+                            >
+                                <BusinessIcon style={{ marginRight: 10 }} />
+                                <Typography>{selectedSOR.address}</Typography>
+                            </div>
+                            <div
+                                style={{
+                                    margin: 10,
+                                    marginBottom: 30,
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                }}
+                            >
+                                <Typography>Przewidywany czas oczekiwania: </Typography>
+                                <Typography style={{ fontWeight: 600, marginLeft: 10 }}>
+                                    {`${selectedSOR.estimatedTime} minut`}
+                                </Typography>
+                            </div>
+                            <Typography style={{ margin: 10 }}>
+                                Liczba pacjentów czekających na SOR:
+                            </Typography>
 
+                            <div
+                                style={{ display: 'flex', flexDirection: 'row', margin: 10 }}
+                            >
+                                {selectedSOR.queueLengths.map((label, index) =>
+                                    index === selectedSOR.queueLengths.length - 1 ? (
+                                        <div
+                                            style={{
+                                                margin: 5,
+                                                display: 'flex',
+                                                flexDirection: 'row',
+                                            }}
+                                        >
+                                            <div>
+                                                <Typography variant="h5">{label}</Typography>
+                                                <Typography style={{ color: 'grey' }}>
+                                                    {times[index]}
+                                                </Typography>
+                                            </div>
+                                        </div>
+                                    ) : (
+                                        <div
+                                            style={{
+                                                margin: 5,
+                                                display: 'flex',
+                                                flexDirection: 'row',
+                                            }}
+                                        >
+                                            <div>
+                                                <Typography variant="h5">{label}</Typography>
+                                                <Typography style={{ color: 'grey' }}>
+                                                    {times[index]}
+                                                </Typography>
+                                            </div>
+                                            <ArrowRightAltIcon />
+                                        </div>
+                                    )
+                                )}
+                            </div>
                         </div>
                     </Popup>
                 ) : null}
